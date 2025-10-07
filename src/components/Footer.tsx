1046b6bd39287cb24e5479ecce402c6df9ac9c29
Yanqin Qualmann <yq.qualmann@gmail.com>
2025-10-07 20:10:59 +0200

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,45 @@
+import { useState, useEffect } from "react";
+import upArrow from "../assets/upArrow.png";
+
 export default function Footer() {
+  const [isTop, setIsTop] = useState<boolean>(false);
+  //console.log(isTop);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 300) {
+        setIsTop(true);
+      } else {
+        setIsTop(false);
+      }
+    });
+  }, []);
+
+  function goTop() {
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  }
+
   return (
-    <footer>
-      <p>© 2023 WTF ?</p>
+    <footer className=" w-full h-16 flex flex-col items-center justify-center space-y-2 relative">
+      {isTop && (
+        <img
+          src={upArrow}
+          className="w-8 z-20 fixed bottom-10 right-10 xl:right-17 xl:bottom-17 drop-shadow-lg hover:-translate-y-1"
+          onClick={goTop}
+          alt="toTop"
+        />
+      )}
+
+      <h5 className="text-sm text-center w-fit h-fit bg-[#ddc783]  text-white mx-auto rounded-full py-0 px-4 drop-shadow-md hover:bg-[#F74211] transition duration-700 ease-in-out">
+        {" "}
+        © {new Date().getFullYear()} by{" "}
+        <a
+          href="https://www.yanqin.de"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          QIN's code
+        </a>
+      </h5>
     </footer>
   );
 }
